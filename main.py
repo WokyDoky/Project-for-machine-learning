8a360e81389903ed9b546b80bddab8e438d803f2
WokyDoky <daniel.benavent3@gmail.com>
2024-12-01 21:49:09 -0700

    Avance

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import math
+import json
+import scipy
+import librosa
+
+
+
+
+def main():
+    print("Hola Mundo!")
+
+
+if __name__ == "__main__":
+    main()
